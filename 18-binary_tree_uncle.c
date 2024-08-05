94b4fb9ba6478e625ab0dc160f899ee9aef21add
Gabriel <gabyrivm06@gmail.com>
2024-08-05 11:10:07 -0400

    task 18, betty style

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,17 +3,17 @@
 /**
  * binary_tree_t *binary_tree_uncle - Finds the uncle of a node.
  * @node: A pointer to the node to find the uncle.
- * 
+ *
  * Return: NULL if node is NULL or if node has no uncle.
  */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
 
-    if (node->parent->parent->left == node->parent)
-        return (node->parent->parent->right);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
 
-    return (node->parent->parent->left);
+	return (node->parent->parent->left);
 }
